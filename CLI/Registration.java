c476216a78f7064df9a16e833b805193e7ba3fa7
isaacforeal <isaacngabirano1@gmail.com>
2022-09-13 14:20:19 +0300

    Create Registration.java

---
+++ CLI/Registration.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Registration {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name password product date_of_birth");
+        String details = sc.nextLine();
+    }
+}
