ce03830a6dcbbef91a5a4d5b7347119dda721bd2
Nelson Ssenkumba <ssenkumbanelson.sn@gmail.com>
2022-09-16 14:47:05 +0300

    Update index.blade.php

--- ANKA/resources/views/Customer/index.blade.php
+++ ANKA/resources/views/Customer/index.blade.php
@@ -1,72 +1,38 @@
 @extends('Layouts.layout')
-@section('content')
-
-<div class="main-panel">
+@section('content') <div class="main-panel">
     <div class="content-wrapper">
-        <div class="row">
-        </div>
+        <div class="row"></div>
         <div class="row">
             <div class="col-md-12 grid-margin">
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex flex-row justify-content-between">
                             <h4 class="card-title">PRODUCTS</h4>
-                            <!-- <p class="text-muted mb-1 small">View all</p> -->
+
                         </div>
                         <div class="preview-list">
+                            @foreach ($products as $pdt)
                             <div class="preview-item border-bottom">
-                                <!-- <div class="preview-thumbnail">
-                          <img src="assets/images/faces/face6.jpg" alt="image" class="rounded-circle" />
-                        </div> -->
                                 <div class="preview-item-content d-flex flex-grow">
                                     <div class="flex-grow">
                                         <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                                            <h6 class="preview-subject">product 1</h6>
+                                            <h6 class="preview-subject">{{ $pdt->product }}</h6>
                                             <button type="button" class="btn btn-outline-success">ORDER</button>
+                                            
+
                                         </div>
-                                        <p class="text-muted">Well, it seems to be working now.</p>
+                                        <p class="text-muted">{{ $pdt->description }}</p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="preview-item border-bottom">
-                                <!-- <div class="preview-thumbnail">
-                          <img src="assets/images/faces/face8.jpg" alt="image" class="rounded-circle" />
-                        </div> -->
-                                <div class="preview-item-content d-flex flex-grow">
-                                    <div class="flex-grow">
-                                        <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                                            <h6 class="preview-subject">product 2</h6>
-                                            <button type="button" class="btn btn-outline-success">ORDER</button>
-                                        </div>
-                                        <p class="text-muted">Well, it seems to be working now.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="preview-item border-bottom">
-                                <!-- <div class="preview-thumbnail">
-                          <img src="assets/images/faces/face9.jpg" alt="image" class="rounded-circle" />
-                        </div> -->
-                                <div class="preview-item-content d-flex flex-grow">
-                                    <div class="flex-grow">
-                                        <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                                            <h6 class="preview-subject">Product 3</h6>
-                                            <button type="button" class="btn btn-outline-success">ORDER</button>
-                                        </div>
-                                        <p class="text-muted">Please review the tickets</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- <div class="preview-item border-bottom">
-                        <div class="preview-thumbnail">
-                          <img src="assets/images/faces/face11.jpg" alt="image" class="rounded-circle" />
-                        </div> -->
+                            @endforeach
+
                             <div class="preview-item-content d-flex flex-grow">
                                 <div class="flex-grow">
                                     <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                                        <h6 class="preview-subject">Product 4</h6>
-                                        <button type="button" class="btn btn-outline-success">ORDER</button>
+                                        
                                     </div>
-                                    <p class="text-muted">Thanks a lot. It was easy to fix it .</p>
+                                    <p class="text-muted"></p>
                                 </div>
                             </div>
                         </div>
@@ -77,7 +43,4 @@
     </div>
 </div>
 
-<!-- content-wrapper ends -->
-<!-- partial:partials/_footer.html -->
 @endsection
-<!-- main-panel ends -->
