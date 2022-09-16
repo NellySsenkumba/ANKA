18f2684cbe5ffd32b4fd27fe47d21b581dbb1338
isaacforeal <isaacngabirano1@gmail.com>
2022-09-16 10:31:01 +0300

    registration.java

--- CLI/Registration.java
+++ CLI/Registration.java
@@ -1,9 +1,23 @@
 import java.util.*;
+import java.io.*;
 
 public class Registration {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter your name password product date_of_birth");
-        String details = sc.nextLine();
+    static String command;
+    public static void main(String[] args) throws IOException {
+    
+        do{
+            String filename = "participant.txt";
+            //PrintWriter p=null;
+            System.out.println("Enter a command");
+            BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+            command=b.readLine();
+            System.out.println("Enter your details in the following order 'name password product date_of_birth'");
+            String details = b.readLine();
+            PrintWriter p  = new PrintWriter(new FileWriter(filename,true),true);
+            p.print(command+" ");
+            p.print(details);
+            p.close();            
+        }while(command!="exit");  
     }
+       
 }
