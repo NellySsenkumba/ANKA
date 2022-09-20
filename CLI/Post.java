95ce3d8566334f505663f51bcc929f7052cd911f
Nelson Ssenkumba <ssenkumbanelson.sn@gmail.com>
2022-09-20 17:28:43 +0300

    Update Post.java

--- CLI/Post.java
+++ CLI/Post.java
@@ -2,27 +2,26 @@ import java.io.*;
 
 public class Post {
     public void post() throws IOException {
-String command;
-        
-            String filename = "products.txt";
-            //PrintWriter p=null;
-            System.out.println("Enter a command");
-            BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
-            command=b.readLine();
-            System.out.println("Post_product product_name description");
-            String details = b.readLine();
+        String command;
 
-            String write="Post_product, product_name, description,0"
-            PrintWriter p  = new PrintWriter(new FileWriter(filename,true),true);
-            p.print(command+" ");
+        String filename = "products.txt";
+        // PrintWriter p=null;
+        System.out.println("Enter a command");
+        BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+        command = b.readLine();
+        System.out.println("Post_product product_name description");
+        String details = b.readLine();
 
-            // String[] blocked=details.split(" ");
-            // for (String bk : blocked) {
-            //     System.out.println(" ");
-            //     System.out.println(bk);
-            //     System.out.println(blocked[0]);
-            // }
-            p.print(details);
-            p.close();            
-        }
+        PrintWriter p = new PrintWriter(new FileWriter(filename, true), true);
+        p.print(command + " ");
+
+        // String[] blocked=details.split(" ");
+        // for (String bk : blocked) {
+        // System.out.println(" ");
+        // System.out.println(bk);
+        // System.out.println(blocked[0]);
+        // }
+        p.print(details);
+        p.close();
+    }
 }
