d6fdbc24f34603be165bbcd460367ea690706f9d
Nelson Ssenkumba <ssenkumbanelson.sn@gmail.com>
2022-09-20 22:40:16 +0300

    cron job 1.0

--- ANKA/app/Console/Kernel.php
+++ ANKA/app/Console/Kernel.php
@@ -16,6 +16,56 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+
+                $users=[];
+                if(($open=fopen(storage_path()."/registration.csv","r+"))!=FALSE){
+                    while(($data=fgetcsv($open))!=FALSE){
+                        $users[]=$data;
+                    }
+    
+                    for($i=1;$i<count($users);$i++){
+                        if ($users[$i][4]=='0'){
+                            
+                        
+                            //insert
+                        
+                            // $pizza=new (new Pizza);
+
+                            // $pizza->name=$users[$i][0];
+                            // $pizza->type=$users[$i][1];
+                            // $pizza->base=$users[$i][2];
+                            // $pizza->toppings=$users[$i][3];
+                            
+                            // $pizza->save();
+                            
+                            // $users[$i][4]='1';
+                       
+    
+                        }
+                    
+    
+                    }
+                
+                
+    
+                    fseek($open,0);
+                    foreach ($users as $row) {
+                    fputcsv($open,$row);
+                    }
+                
+                
+                    fclose($open);
+    
+    
+                }
+    
+    
+    
+           
+            })->everyMinute();
+
+        
     }
 
     /**
--- CLI/Registration.java
+++ CLI/Registration.java
@@ -2,32 +2,30 @@ import java.util.*;
 import java.io.*;
 
 public class Registration {
-    static  String command;
-    
+    static String command;
+
     public static void main(String[] args) throws IOException {
-        
-            String filename = "D:/xampp/htdocs/ANKA/ANKA/storage/registration.csv";
-            //PrintWriter p=null;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter your details in the following order 'command name password product date_of_birth'");
-            String command = sc.next();
-            String name = sc.next();
-            String password = sc.next();
-            String product = sc.next();
-            String date_of_birth=sc.next();
-            PrintWriter p  = new PrintWriter(new FileWriter(filename,true),true);
-            p.print(name+","+password+","+product+","+date_of_birth);
-            p.close();            
-       
-  
+
+        String filename = "D:/xampp/htdocs/ANKA/ANKA/storage/registration.csv";
+        // PrintWriter p=null;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your details in the following order 'command name password product date_of_birth'");
+        String command = sc.next();
+        String name = sc.next();
+        String password = sc.next();
+        String product = sc.next();
+        String date_of_birth = sc.next();
+        PrintWriter p = new PrintWriter(new FileWriter(filename, true), true);
+        p.print(name + "," + password + "," + product + "," + date_of_birth + "0");
+        p.close();
+
     }
-       
+
 }
 
-
-//converting string to an array
-            //String [] blocked=details.split(" ");
-            // for (String bk : blocked) {
-            //     System.out.println(" ");
-            //     System.out.println(bk);
-            // }
+// converting string to an array
+// String [] blocked=details.split(" ");
+// for (String bk : blocked) {
+// System.out.println(" ");
+// System.out.println(bk);
+// }
