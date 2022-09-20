1fc7cc5f7f638550ada2200230113d641ab99df8
isaacforeal <isaacngabirano1@gmail.com>
2022-09-20 18:23:13 +0300

    new

--- CLI/Registration.java
+++ CLI/Registration.java
@@ -4,22 +4,19 @@ import java.io.*;
 public class Registration {
     static  String command;
     
-    public void register() throws IOException {
+    public static void main(String[] args) throws IOException {
         
-            String filename = "registration.txt";
+            String filename = "D:/xampp/htdocs/ANKA/ANKA/storage/registration.csv";
             //PrintWriter p=null;
-            BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+            Scanner sc = new Scanner(System.in);
             System.out.println("Enter your details in the following order 'command name password product date_of_birth'");
-            String details = b.readLine();
+            String command = sc.next();
+            String name = sc.next();
+            String password = sc.next();
+            String product = sc.next();
+            String date_of_birth=sc.next();
             PrintWriter p  = new PrintWriter(new FileWriter(filename,true),true);
-            p.print(details);
-
-            //converting string to an array
-            //String [] blocked=details.split(" ");
-            // for (String bk : blocked) {
-            //     System.out.println(" ");
-            //     System.out.println(bk);
-            // }
+            p.print(name+","+password+","+product+","+date_of_birth);
             p.close();            
        
   
@@ -28,3 +25,9 @@ public class Registration {
 }
 
 
+//converting string to an array
+            //String [] blocked=details.split(" ");
+            // for (String bk : blocked) {
+            //     System.out.println(" ");
+            //     System.out.println(bk);
+            // }
--- README.md
+++
@@ -1,2 +0,0 @@
-# ANKA
-GROUP 33
