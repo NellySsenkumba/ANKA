39aca1f0c0b17b7f702fffca8ce9d0fe7806101e
isaacforeal <isaacngabirano1@gmail.com>
2022-09-21 10:08:29 +0300

    nn

--- CLI/Registration.java
+++ CLI/Registration.java
@@ -4,25 +4,6 @@ import java.security.Timestamp;
 import java.time.*;
 
 public class Registration {
-<<<<<<< Updated upstream
-    static String command;
-
-    public static void main(String[] args) throws IOException {
-
-        String filename = "D:/xampp/htdocs/ANKA/ANKA/storage/registration.csv";
-        // PrintWriter p=null;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your details in the following order 'command name password product date_of_birth'");
-        String command = sc.next();
-        String name = sc.next();
-        String password = sc.next();
-        String product = sc.next();
-        String date_of_birth = sc.next();
-        PrintWriter p = new PrintWriter(new FileWriter(filename, true), true);
-        p.print(name + "," + password + "," + product + "," + date_of_birth + ",0");
-        p.close();
-
-=======
     public static void menu(){
         System.out.println("register- adds you to participants");
         System.out.println("exit-takes you to the menu");
@@ -35,10 +16,11 @@ public class Registration {
         
             String filename = "D:/xampp/htdocs/ANKA/ANKA/storage/registration.csv";
             //PrintWriter p=null;
+            LocalTime t = LocalTime.now();
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter your details in the following order 'command name password product date_of_birth'");
             String command = sc.next();
-            //Timestamp t = new Timestamp(null, null);
+            
 
             if(command.equalsIgnoreCase("register")&&!command.equalsIgnoreCase("exit")){
                 int update=0;
@@ -47,13 +29,12 @@ public class Registration {
                 String product = sc.next();
                 String date_of_birth=sc.next();
             PrintWriter p  = new PrintWriter(new FileWriter(filename,true),true);
-            p.println(name+","+password+","+product+","+date_of_birth+","+update);
+            p.println(name+","+password+","+product+","+date_of_birth+","+update+","+t);
             p.close();            
             }else{
                 menu();
             }
   
->>>>>>> Stashed changes
     }
 
 }
---
+++ CLI/Time.java
@@ -0,0 +1,8 @@
+import java.time.LocalTime; // import the LocalTime class
+
+public class Time {
+  public static void main(String[] args) {
+    LocalTime t = LocalTime.now();
+    System.out.println(myObj);
+  }
+}
