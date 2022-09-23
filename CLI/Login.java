ed460914e77af87ec05d34829fbdd0e3fe1ee4ec
isaacforeal <isaacngabirano1@gmail.com>
2022-09-23 19:53:48 +0300

    new

--- CLI/Login.java
+++ CLI/Login.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-import java.time.LocalTime;
+
 
 public class Login {
     static Scanner scan = new Scanner(System.in);
@@ -37,45 +37,45 @@ public class Login {
   pwd.close();       
  //---------------------------------------------------------------------
  
- File myFile2=new File("./ANKA/storage/results.csv");
- BufferedReader buf = new BufferedReader(new FileReader(myFile2));
- Object[] lines = buf.lines().toArray();
- int counter = 0;
-        for(int count=0; count<lines.length;count++){
-            counter++;
+//  File myFile2=new File("./ANKA/storage/results.csv");
+//  BufferedReader buf = new BufferedReader(new FileReader(myFile2));
+//  Object[] lines = buf.lines().toArray();
+//  int counter = 0;
+//         for(int count=0; count<lines.length;count++){
+//             counter++;
             
-        }
+//         }
  
- for(int i=0; i<lines.length; i++){
-     String line = lines[i].toString().trim();
-     String[] column =  line.split(",");
+//  for(int i=0; i<lines.length; i++){
+//      String line = lines[i].toString().trim();
+//      String[] column =  line.split(",");
      
-    if(column[0].equalsIgnoreCase(username)){
-     Main ma = new Main();
-     System.out.println("your information will be available at ");
-     System.out.println("");
-     System.out.println("THIS IS YOUR INFORMATION");
-     System.out.println("----------------------------------");
-     System.out.println("| name          | "+column[0]+"            |");
-     System.out.println("----------------------------------");
-     System.out.println("| points        | "+column[1]+"              |");
-     System.out.println("----------------------------------");
-     System.out.println("| rank          | "+column[2]+" outof "+counter+"      |");
-     System.out.println("----------------------------------");
-     System.out.println("| buyers        | "+column[3]+"|");
-     System.out.println("----------------------------------");
-     System.out.println("| products left | "+column[4]+"|");
-     System.out.println("----------------------------------");
+//     if(column[0].equalsIgnoreCase(username)){
+//      Main ma = new Main();
+//      System.out.println("your information will be available at ");
+//      System.out.println("");
+//      System.out.println("THIS IS YOUR INFORMATION");
+//      System.out.println("----------------------------------");
+//      System.out.println("| name          | "+column[0]+"            |");
+//      System.out.println("----------------------------------");
+//      System.out.println("| points        | "+column[1]+"              |");
+//      System.out.println("----------------------------------");
+//      System.out.println("| rank          | "+column[2]+" outof "+counter+"      |");
+//      System.out.println("----------------------------------");
+//      System.out.println("| buyers        | "+column[3]+"|");
+//      System.out.println("----------------------------------");
+//      System.out.println("| products left | "+column[4]+"|");
+//      System.out.println("----------------------------------");
      
-    }
- }
+//     }
+//  }
  
  //---------------------------------------------------------------------------------
  
         }  
         else{
             System.out.println("Login Failed");
-            
+            System.exit(0);
         }
         Main m = new Main();
          
--- CLI/Main.java
+++ CLI/Main.java
@@ -72,55 +72,101 @@ public  void command() throws IOException, FileNotFoundException{
             
             String filename2 = "./ANKA/storage/product.csv";
 
-            File myFile=new File("./ANKA/storage/product.csv");
+            File myFile=new File("./ANKA/storage/registration.csv");
             BufferedReader br = new BufferedReader(new FileReader(myFile));
             Object[] line = br.lines().toArray();
             br.close();
-            int exist = 0;
+            int exist = 1;
             for(int i=0; i<line.length; i++){
                 String lines = line[i].toString().trim();
                 String[] column =  lines.split(",");
                 
-            //     if(pro_name.equals(column[0])){
-            //         exist = 1; 
-            //         System.out.println("product already posted");
-            //         command();
-            //     }
+                if(!pro_name.equals(column[2])){
+                    exist = 0; 
+                    System.out.println("product cant be posted since its not registered");
+                    command();
+                }
                
-            // }
-            // if(exist==0){
+            }
+            if(exist==1){
 
             PrintWriter w = new PrintWriter(new FileWriter(filename2, true), true);
             w.println(pro_name + ", " + pro_qt + ", " + pro_px + ", " + pro_desc + ", " + Updated);
             
             w.close();
             command();
-            }
+           }
    }
    
    //Performance.....
    else if(command.equalsIgnoreCase("performance")&&!command.equalsIgnoreCase("exit")){
-            LocalTime request = LocalTime.now();    
-            Scanner so = new Scanner(System.in);
-            System.out.println("To see your results type view");
-            String view=so.nextLine();
+                LocalTime request = LocalTime.now();    
+            
+                System.out.println("please login first");
+                Login n = new Login();
+                n.login(sc);
 
-   if(view.equalsIgnoreCase("view")){
-            LocalTime seen = LocalTime.now();
-            System.out.println("please login first");
-            Login n = new Login();
-            n.login(so);
-            response = request.plusMinutes(1);
+                System.out.println("To see your results type view");
+                // Scanner so = new Scanner(System.in);
+                // String view=so.nextLine();
+                BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+                String view = b.readLine();
+
+    if(view.equalsIgnoreCase("view")){
+
+
+//--------------------------------------------------------------------
+ 
+ File myFile2=new File("./ANKA/storage/results.csv");
+ BufferedReader buf = new BufferedReader(new FileReader(myFile2));
+ Object[] lines = buf.lines().toArray();
+ int counter = 0;
+        for(int count=0; count<lines.length;count++){
+            counter++;
+            
+        }
+ buf.close();
+ for(int i=0; i<lines.length; i++){
+     String line = lines[i].toString().trim();
+     String[] column =  line.split(",");
+     Login ol = new Login();
+     
+    if(column[0].equalsIgnoreCase(ol.username)){
+     //Main ma = new Main();
+     System.out.println("your information will be available at ");
+     System.out.println("");
+     System.out.println("THIS IS YOUR INFORMATION");
+     System.out.println("----------------------------------");
+     System.out.println("| name          | "+column[0]+"            |");
+     System.out.println("----------------------------------");
+     System.out.println("| points        | "+column[1]+"              |");
+     System.out.println("----------------------------------");
+     System.out.println("| rank          | "+column[2]+" outof "+counter+"      |");
+     System.out.println("----------------------------------");
+     System.out.println("| buyers        | "+column[3]+"|");
+     System.out.println("----------------------------------");
+     System.out.println("| products left | "+column[4]+"|");
+     System.out.println("----------------------------------");
+     
+    }
+ }
+ 
+ //---------------------------------------------------------------------------------
+
+                 LocalTime seen = LocalTime.now();
+                 response = request.plusMinutes(1);
 
 
 File time =new File("./ANKA/storage/time.csv");
 PrintWriter op = new PrintWriter(new FileWriter(time, true), true);
-op.println(n.username+","+request+","+seen+","+response+","+0);
+op.println(n.username+","+request+","+seen+","+response+","+1);
 op.close();
 
 
 
    }
+     // so.close();
+     //b.close();      
 
             }
         }
